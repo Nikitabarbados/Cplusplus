10b890c9783515a211dafc5b01620cc5a4665cad
Nikitabarbados <nikita.gaber2703@gmail.com>
2025-03-13 21:48:01 +0200

    DZ

---
+++ Console 1.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, ""); // включити кириллицю
+
+    cout << "Exercise_1\n\n\n";
+
+    cout << "To be\n\n";
+    cout << "\tor not\n\n";
+    cout << "\t\tto be...\n\n";
+    cout << "\t\t\t\\ William Shakespeare \\ \n\n\n\n";
+    // \t - табуляція
+
+    cout << "Exercise_2\n\n\n";
+
+    cout << "    Березень 2025\n";
+    cout << " ПН ВТ СР ЧТ ПТ СБ НД\n";
+    cout << "                 1  2\n";
+    cout << "  3  4  5  6  7  8  9\n";
+    cout << " 10 11 12 13 14 15 16\n";
+    cout << " 17 18 19 20 21 22 23\n";
+    cout << " 24 25 26 27 28 29 30\n";
+    cout << " 31\n\n\n\n";
+
+    cout << "Exercise_3\n\n\n";
+
+    cout << "        *****                               \n";
+    cout << "        *   *                               \n";
+    cout << "      *****************                     \n";
+    cout << "     *                 *                    \n";
+    cout << "    *                   *                   \n";
+    cout << "   *                     *                  \n";
+    cout << "   ***********************                  \n";
+    cout << "   *                     *                  \n";
+    cout << "   *   ******   ******   *     /     /     /\n";
+    cout << "   *   *    *   *    *   *//////////////////\n";
+    cout << "   *   *    *   *   **   *     /     /     /\n";
+    cout << "   *   ******   *    *   *//////////////////\n";
+    cout << "   *            *    *   *     /     /     /\n";
+
+    cout << "Exercise_4\n\n\n";
+
+    cout << "                                                         РЕЗЮМЕ\n\n";
+    cout << "---------------------------------------------------------------------------------------------------------------------------------\n";
+    cout << "                    Nikita Gaber                         |\n";
+    cout << "                                                         |\n";
+    cout << "                                                         |                              Skills\n";
+    cout << "                                                         |\n";
+    cout << "                                                         |           C++ | Python | HTML | CSS | JavaScript |\n";
+    cout << "                                                         |\n";
+    cout << "                                                         |           HTML | CSS | Bootstrap | Java Script | Type Script |\n";
+    cout << "                                                         |           React |\n";
+    cout << "                                                         |\n";
+    cout << "                                                         |           Visual Studio | Visual Studio Code |\n";
+    cout << "                                                         |           Git | Git Hub |\n";
+    cout << "---------------------------------------------------------|\n";
+    cout << "    nikita.gaber2703@gmail.com                           |-----------------------------------------------------------------------\n";
+    cout << "    +380 123 456 78                                      |\n";
+    cout << "                                                         |                           CERTIFICATES\n";
+    cout << "    Sofia, Bulgaria                                      |\n";
+    cout << "                                                         |           CISCO: IT Essentials PC Hardware and Software\n";
+    cout << "    27.03.2009                                           |\n";
+    cout << "                                                         |-----------------------------------------------------------------------\n";
+    cout << "---------------------------------------------------------|                            LANGUAGES\n";
+    cout << "               EDUCETION                                 |\n";
+    cout << "                                                         |           English (B2)                 * 0 0 0 0\n";
+    cout << "    Christian Humanitarian and Economic                  |\n";
+    cout << "                                                         |           Bulgaria (B2)                * * * 0 0\n";
+    cout << "    Open University                                      |\n";
+    cout << "    Department of Administrative  and Commercial Law     |           Ukrainian                    * * * * *\n";
+    cout << "    2002 - 2007                                          |\n";
+    cout << "                                                         |           Russian                      * * * * *\n";
+    cout << "                                                         |\n";
+    cout << "    IT STEP Academy                                      |-----------------------------------------------------------------------\n";
+    cout << "    Software Development Department                      |\n";
+    cout << "                                                         |                            PROJECTS\n";
+    cout << "    2022 - 05/2025                                       |\n";
+    cout << "                                                         |           Booking\n";
+    cout << "---------------------------------------------------------|           10/2024 - 01/2025\n";
+    cout << "                                                         |\n";
+    cout << "         PROFESSIONAL EXPERIENCE                         |           The primary objective of the project is to develop a\n";
+    cout << "                                                         |           convenient, fast, and reliable accommodation booking\n";
+    cout << "    Veles Agro Ltd.                                      |           service, while also fostering effective teamwork and\n";
+    cout << "    Brand manager                                        |           collaboration. My specific contribution focused on creating\n";
+    cout << "    2015 - 2018                                          |           the server-side functionality, ensuring seamless\n";
+    cout << "                                                         |           communication between the front-end and back-end\n";
+    cout << "                                                         |           components.\n";
+    cout << "    Veles Agro Ltd.                                      |           This involved designing and implementing a robust API,\n";
+    cout << "    Regional director                                    |           managing database interactions, and ensuring that the\n";
+    cout << "    2018 - 2019                                          |           system was scalable and secure. I prioritized performance\n";
+    cout << "                                                         |           optimization and error handling to guarantee a smooth\n";
+    cout << "                                                         |           user experience. Additionally, I collaborated closely with\n";
+    cout << "    Veles Agro Ltd.                                      |           team members to align on requirements, integrate features,\n";
+    cout << "    Sales Director                                       |           and troubleshoot issues, contributing to the overall success\n";
+    cout << "    2019 - 2023                                          |           of the project.\n";
+    cout << "                                                         |-----------------------------------------------------------------------\n";
+    cout << "                                                         |           https://github.com/Nikitabarbados\n";
+}
