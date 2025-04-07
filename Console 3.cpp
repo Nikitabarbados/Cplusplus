bf95fcf088a7ea7a9d7bea24d8ed7a721023e745
Nikitabarbados <nikita.gaber2703@gmail.com>
2025-04-08 00:18:12 +0300

    ДЗ

---
+++ Console 3.cpp
@@ -0,0 +1,101 @@
+﻿// 1 Exercise
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    double a, b;
+//    cout << "Enter first number: ";
+//    cin >> a;
+//    cout << "Enter second number: ";
+//    cin >> b;
+//
+//    cout << "Summa: " << a + b << "\n";
+//    cout << "Difference: " << a - b << "\n";
+//    cout << "Proizvedenie: " << a * b << "\n";
+//}
+
+// 2 Exercise
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double number, percent;
+//    cout << "Enter number: ";
+//    cin >> number;
+//    cout << "Enter a percent: ";
+//    cin >> percent;
+//
+//    double result = number * (percent / 100);
+//    cout << percent << "% from " << number << " = " << result;
+//}
+
+// 3 Exercise
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double width, height;
+//    cout << "Enter width rectangle: ";
+//    cin >> width;
+//    cout << "Enter height rectangle: ";
+//    cin >> height;
+//
+//    double area = width * height;
+//    cout << "Rectangle: " << area;
+//}
+
+// 4 Exercise
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double celsius;
+//    cout << "Enter temperature in Celsius: ";
+//    cin >> celsius;
+//
+//    double fahrenheit = (celsius * 9 / 5) + 32;
+//    cout << "Temperature in Fahrenheit: " << fahrenheit;
+//}
+
+// 5 Exercise
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double a, b, c;
+//    cout << "Enter the first fractional number: ";
+//    cin >> a;
+//    cout << "Enter the second fractional number: ";
+//	cin >> b;
+//    cout << "Enter the third fractional number: ";
+//	cin >> c;
+//
+//    double average = (a + b + c) / 3;
+//    cout << "Arithmetic average: " << average;
+//}
+
+// 6 Exercise
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    double radius;
+//    double PI = 3.14159;
+//    
+//    cout << "Enter the radius circle: ";
+//    cin >> radius;
+//
+//    double area = PI * radius * radius;
+//    double length = 2 * PI * radius;
+//
+//    cout << "Area circle: " << area;
+//    cout << "Length circle: " << length;
+//}
+
