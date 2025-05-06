ec81f84ef7a1b378f7dddba89f59d69ad0a6e2bc
Nikitabarbados <nikita.gaber2703@gmail.com>
2025-05-06 16:30:07 +0300

    DZ
    DZ

---
+++ Console 15.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void obmin(int* a, int* b) {
+    int temp = *a; 
+    *a = *b;     
+    *b = temp;    
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    obmin(&x, &y); 
+    cout << x << " " << y << "\n"; 
+
+}
