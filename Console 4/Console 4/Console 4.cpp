51e17a19a6a608b1cf45dbd5c5317f194c9ce5f2
Nikita <nikita.gaber2703@gmail.com>
2026-01-28 22:10:14 +0200

    Add files via upload

---
+++ Console 4/Console 4/Console 4.cpp
@@ -0,0 +1,13 @@
+﻿#include "FileName1.h"
+using namespace std;
+int main() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Summa: " << a + b << "\n";
+    cout << "Difference: " << a - b << "\n";
+    cout << "Proizvedenie: " << a * b << "\n";
+}
---
+++ Console 4/Console 4/FileName1.h
@@ -0,0 +1,11 @@
+
+#include <iostream> // 1) бібліотеки
+#include <windows.h>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <conio.h>
+using namespace std;
+
